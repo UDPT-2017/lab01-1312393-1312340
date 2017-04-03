0474b9d9f20abb41ef425c70a6c03dc3f553b93f
DESKTOP-69P04H1\User <tanlokami@gmail.com>
2017-04-03 22:45:20 +0700

    Revert "Update Album and Album detail!"
    This reverts commit a1068f1cb73cf417795b43b65358379a941e00fb.

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.get("/album",function(req,res){
 app.get("/blog",function(req,res){
   res.render('blog');
 });
+app.get("/nguyenalbum",function(req,res){
+  res.render('nguyenalbum');
+});
+app.get("/localbum",function(req,res){
+  res.render('localbum');
+});
+
 
 var port=3001
 app.listen(port,function(){
